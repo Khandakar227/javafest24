67457df2d1836fddc0f505a5993510f490d4c7ff
Anonna <meftahul@iut-dhaka.edu>
2024-08-12 23:11:55 +0600

    icon+ui add

--- client/pages/medicines/[slug].tsx
+++ client/pages/medicines/[slug].tsx
@@ -5,10 +5,49 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+
+type DosageForm =
+  | "Tablet"
+  | "Mouthwash"
+  | "Injection"
+  | "Capsule"
+  | "Cream"
+  | "Dialysis Solution"
+  | "Eye Drop"
+  | "Gel"
+  | "Syrup"|"Emulsion";
+const dosageFormIcons: Record<DosageForm, string> = {
+  Tablet: "/images-med/tablet.png",
+  Mouthwash: "/images-med/mouthwash.png",
+  Injection: "/images-med/injection.png",
+  Capsule: "/images-med/capsule.png",
+  Cream: "/images-med/cream.png",
+  "Dialysis Solution": "/images-med/dialysis-solution.png",
+  "Eye Drop": "/images-med/eye-drop.png",
+  "Gel": "/images-med/gel.png",
+ "Syrup": "/images-med/syrup.png",
+  "Emulsion":"/images-med/emulsion.png"
+};
+const mapDosageForm = (str: string) => {
+  if (/Injection/.test(str)) {
+    return 'Injection';
+  } else if (/Gel/.test(str)) {
+    return 'Gel';
+  } else if (/Emulsion|Suspension|Powder/.test(str)) {
+    return 'Emulsion';
+  }
+ 
+  return str;
+};
+
+function getIconUrl(dosageForm: DosageForm): string {
+  return dosageFormIcons[dosageForm] || "";
+}
 
 export default function Slug() {
   const router = useRouter();
-  
+
   const [medicine, setMedicine] = useState<{
     medicine: Medicine;
     generic: Generic;
@@ -22,7 +61,7 @@ export default function Slug() {
       })
       .catch((err) => console.log(err));
   }, [router.query.slug]);
-
+  const transformedDosageForm = medicine ? mapDosageForm(medicine.medicine.dosageForm) : "";
   return (
     <>
       <Head>
@@ -33,25 +72,39 @@ export default function Slug() {
           {medicine && medicine.medicine && (
             <>
               <div className="py-4">
-                <h1
-                  className="text-3xl font-bold"
-                  style={{
-                    marginRight: `calc(12px + 0.4vw)`,
-                    fontSize: "27px",
-                    color: "#435b66",
-                  }}
-                >
-                  {medicine.medicine.brandName}
-                  <small
-                    className="h1-subtitle font-normal"
+              
+                <div className="flex items-center">
+                  <h1
+                    className="text-3xl font-bold"
                     style={{
-                      fontSize: "15px",
-                      color: "#777777",
+                      marginRight: `calc(12px + 0.4vw)`,
+                      fontSize: "27px",
+                      color: "#435b66",
                     }}
                   >
-                    {medicine.medicine.dosageForm}
-                  </small>
-                </h1>
+                    <span className="mr-2 md-icon-container mt-neg no-mr">
+                      <Image
+                        src={getIconUrl(transformedDosageForm)}
+                        alt={medicine.medicine.dosageForm}
+                        title={medicine.medicine.dosageForm}
+                        width={31}
+                        height={31}
+                      />
+                    </span>
+
+                    {medicine.medicine.brandName}
+                    <small
+                      className="h1-subtitle font-normal"
+                      style={{
+                        fontSize: "15px",
+                        color: "#777777",
+                        paddingLeft: "5px",
+                      }}
+                    >
+                      {medicine.medicine.dosageForm}
+                    </small>
+                  </h1>
+                </div>
                 <p className="pt-4" style={{ color: "#306976" }}>
                   {medicine.medicine.generic}
                 </p>
@@ -61,27 +114,30 @@ export default function Slug() {
                 </p>
               </div>
               <div className="py-6">
-                <p style={{ color: "#3a5571" }}>{medicine.medicine.price.replace(/,/g, ' ')}</p>
+                <p style={{ color: "#3a5571" }}>
+                  {medicine.medicine.price.replace(/,/g, " ")}
+                </p>
               </div>
-             
-
               <div className="similar-brands inline-block mt-10 margin-b-2">
-                  <Link
-                   
-                    href={`/medicines/alternative/${medicine.medicine.generic.replace(/\s+/g, '-')}`}
-                    passHref
-                  >
-                    <p className="btn custom-btn">Alternative Brands</p>
-                  </Link>
-               
+                <Link
+                  href={`/medicines/alternative/${medicine.medicine.generic.replace(
+                    /\s+/g,
+                    "-"
+                  )}`}
+                  passHref
+                >
+                  <p className="btn custom-btn">Alternative Brands</p>
+                </Link>
               </div>
 
               {medicine.generic && (
                 <div className="py-6">
-                   <div className="py-2">
-                <p className="ac-header">Type</p>
-                <p className="ac-body">{medicine.medicine.type.toUpperCase()}</p>
-              </div>
+                  <div className="py-2">
+                    <p className="ac-header">Type</p>
+                    <p className="ac-body">
+                      {medicine.medicine.type.toUpperCase()}
+                    </p>
+                  </div>
                   {medicine.generic.drugClass && (
                     <div className="py-2">
                       <h3 className="ac-header">Drug Class</h3>
--- client/pages/medicines/index.tsx
+++ client/pages/medicines/index.tsx
@@ -8,81 +8,127 @@ import { FiSearch } from "react-icons/fi";
 import ReactPaginate from "react-paginate";
 
 export default function Medicines() {
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState("");
   const [page, setPage] = useState(0);
   const [totalPages, setTotalPage] = useState(0);
   const [medicines, setMedicines] = useState([] as Medicine[]);
 
   useEffect(() => {
-    getMedicines(keyword, page)
-      .then(res => {
-        console.log(res);
-        setTotalPage(res.totalPages);
-        setMedicines(res.content || []);
-      })
-  }, [keyword, page])
+    getMedicines(keyword, page).then((res) => {
+      console.log(res);
+      setTotalPage(res.totalPages);
+      setMedicines(res.content || []);
+    });
+  }, [keyword, page]);
 
-
-  function onPageChange(selectedItem: { selected: number; }) {
-    console.log("selectedItem ", selectedItem)
+  function onPageChange(selectedItem: { selected: number }) {
+    console.log("selectedItem ", selectedItem);
     setPage(selectedItem.selected);
   }
 
-  function onSearch(e:FormEvent) {
+  function onSearch(e: FormEvent) {
     e.preventDefault();
   }
 
-  function onSearchBarChange(e:ChangeEvent) {
+  function onSearchBarChange(e: ChangeEvent) {
     setKeyword((e.target as HTMLInputElement).value);
   }
   return (
     <>
       <Head>
         <title>Medicines - Cerena</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&family=Montserrat:wght@700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Layout>
-        <div className={`shadow rounded-md px-4 py-12 m-4 bg-white text-center`}>
-          <h1 className="font-bold md:text-3xl text-2xl">Know about what medicines your taking</h1>
-          <form onSubmit={onSearch} className='max-w-4xl mx-auto flex gap-4 justify-center items-center py-5'>
-            <input value={keyword} onChange={onSearchBarChange} type="search" name="keyword" id="search-medicine" placeholder="Look for medicine..." className='px-2 py-1 rounded shadow border w-full outline-none focus:outline-primary' />
-            <button className='bg-primary p-2 rounded'><FiSearch size={20} /></button>
+        <div
+          className={`shadow rounded-md px-4 py-12 m-4 bg-white text-center`}
+        >
+          <h1
+            className="font-bold md:text-3xl text-2xl text-gray-700"
+            style={{ fontFamily: "Montserrat, sans-serif" }}
+          >
+            What's in Your Prescription?
+          </h1>
+          <form
+            onSubmit={onSearch}
+            className="max-w-4xl mx-auto flex gap-4 justify-center items-center py-5"
+          >
+            <input
+              value={keyword}
+              onChange={onSearchBarChange}
+              type="search"
+              name="keyword"
+              id="search-medicine"
+              placeholder="Look for medicine..."
+              className="px-2 py-1 rounded shadow border w-full outline-none focus:outline-primary"
+            />
+            <button className="bg-primary p-2 rounded">
+              <FiSearch size={20} />
+            </button>
           </form>
 
-          <div className="py-4">
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 justify-center items-center gap-4">
-              {
-                medicines.map(medicine =>
-                  <div className="shadow rounded text-left p-4" key={medicine.id}>
-                    <Link href={`/medicines/${medicine.slug}`} className="block">
-                      <h1 className="font-semibold text-xl">{medicine.brandName} {medicine.dosageForm}</h1>
-                      <p className="text-xs text-gray-600">{medicine.generic}</p>
-                      <p className="text-sm py-1">Manufacturer: {medicine.manufacturer}</p>
-                      <p className="pt-2">{medicine.price}</p>
-                    </Link>
-                  </div>)
-              }
-            </div>
-            
-            <div className="pt-8">
-                    <ReactPaginate
-                    className="flex justify-center items-center gap-4 flex-wrap"
-                    pageLinkClassName={'px-4 py-2 rounded-md shadow outline-none bg-primary text-white text-sm hover:bg-green-700 hover:text-white'}
-                    pageCount={totalPages}
-                    breakLabel="..."
-                    nextLabel=">"
-                    previousLinkClassName="px-4 py-2 rounded-md outline-none hover:bg-green-700 hover:text-white"
-                    nextLinkClassName="px-4 py-2 rounded-md outline-none hover:bg-green-700 hover:text-white"
-                    pageRangeDisplayed={5}
-                    previousLabel="<"
-                    renderOnZeroPageCount={null}
-                    activeLinkClassName="!bg-green-700 !text-white"
-                    initialPage={page}
-                    onPageChange={onPageChange}
-                    />
+          <div className="py-4 px-10">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 justify-center items-center gap-4 ">
+              {medicines.map((medicine) => (
+                <div
+                  className="shadow rounded text-left p-4 flex-col justify-between h-[170px]  grid-custom"
+                  key={medicine.id}
+                >
+                  <Link href={`/medicines/${medicine.slug}`} className="block">
+                    <h1
+                      className="font-semibold text-xl"
+                      style={{
+                        marginRight: `calc(12px + 0.4vw)`,
+                        fontSize: "25px",
+                        color: "#435b66",
+                      }}
+                    >
+                      {medicine.brandName}{" "}
+                      <small
+                        className="h1-subtitle font-normal"
+                        style={{
+                          fontSize: "15px",
+                          color: "#777777",
+                        }}
+                      >
+                        {medicine.dosageForm}
+                      </small>
+                    </h1>
+                    <p className="text-xs text-gray-600">{medicine.generic}</p>
+                    <p className="text-sm py-1">{medicine.manufacturer}</p>
+                    <p className={`pt-2 text-gray-600`}>
+                      {medicine.price.replace(/,/g, " ").replace(/:/g, " ")}
+                    </p>
+                  </Link>
                 </div>
+              ))}
+            </div>
+
+            <div className="pt-8">
+              <ReactPaginate
+                className="flex justify-center items-center gap-4 flex-wrap"
+                pageLinkClassName={
+                  "px-4 py-2 rounded-md shadow outline-none bg-primary text-white text-sm hover:bg-green-700 hover:text-white"
+                }
+                pageCount={totalPages}
+                breakLabel="..."
+                nextLabel=">"
+                previousLinkClassName="px-4 py-2 rounded-md outline-none hover:bg-green-700 hover:text-white"
+                nextLinkClassName="px-4 py-2 rounded-md outline-none hover:bg-green-700 hover:text-white"
+                pageRangeDisplayed={5}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+                activeLinkClassName="!bg-green-700 !text-white"
+                initialPage={page}
+                onPageChange={onPageChange}
+              />
+            </div>
           </div>
         </div>
       </Layout>
     </>
-  )
+  );
 }
