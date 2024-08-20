ab6afd13315335445a3003ddfa9c553b477f7d4c
Anonna <meftahul@iut-dhaka.edu>
2024-08-20 06:20:35 +0600

    ui update

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,6 +3,8 @@ import { Variants, motion } from "framer-motion"
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const variants: Variants = {
   open: { opacity: 1, x: 0 },
@@ -33,8 +35,16 @@ export default function Layout({ children }: PropsWithChildren) {
         </div>
         <div className="flex-auto bg-slate-100 min-h-screen">
           {children}
+          <footer className="bg-[#71C171] text-gray-600 hover:text-white px-6 py-4 mt-8">
+      
+       
+        <Link href="/"><Image src={'/cerena-logo.png'} alt="Cerena" width={45} height={45} /></Link>
+        <p>&copy; {new Date().getFullYear()} Cerena. All rights reserved.</p>
+    
+    </footer>
         </div>
       </div>
     </div>
+    
   )
 }
