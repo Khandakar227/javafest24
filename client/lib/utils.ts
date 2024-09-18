deaa4103e0b0a7e43961edb11c4f994466999788
Anonna <meftahul@iut-dhaka.edu>
2024-09-19 03:42:40 +0600

    big screen icon bugs

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,21 +1,31 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { FaUserDoctor } from "react-icons/fa6";
+import { GiMedicines ,GiBackPain} from "react-icons/gi";
+import { IoIosFitness } from "react-icons/io";
+import { BsFillCalculatorFill } from "react-icons/bs";
+import { BiDonateBlood } from "react-icons/bi";
+import { FaHandsAslInterpreting } from "react-icons/fa6";
+import { LuGanttChartSquare } from "react-icons/lu";
+import { MdOutlineFoodBank } from "react-icons/md";
+import { MdOutlineDocumentScanner } from "react-icons/md";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const navsmenu = [
-    { label: 'Doctors', href: '/doctors' },
-    { label: 'Medicines', href: '/medicines' },
-    { label: 'Fitness and Exercise', href: '/fitness-and-exercise' },
-    { label: 'BMI Calculator', href: '/bmi-calculator' },
-    { label: 'Disease Prediction', href: '/disease-prediction' },
-    { label: 'Blood Bank', href: '/blood-bank' },
-    { label: 'Sign Speak', href: '/signspeak' },
-    { label: 'Diet Planner', href: '/diet-plan' },
-    { label: 'Food Calorie', href: '/food-calorie' },
-    { label: 'Doc Scanner', href: '/scanner' },
+    { label: 'Doctors', href: '/doctors',icon:FaUserDoctor },
+    { label: 'Medicines', href: '/medicines',icon:GiMedicines },
+    { label: 'Fitness and Exercise', href: '/fitness-and-exercise',icon:IoIosFitness},
+    { label: 'BMI Calculator', href: '/bmi-calculator',icon:BsFillCalculatorFill },
+    { label: 'Disease Prediction', href: '/disease-prediction',icon:GiBackPain },
+    { label: 'Blood Bank', href: '/blood-bank' ,icon:BiDonateBlood},
+    { label: 'Sign Speak', href: '/signspeak',icon:FaHandsAslInterpreting },
+    { label: 'Diet Planner', href: '/diet-plan',icon:LuGanttChartSquare },
+    { label: 'Food Calorie', href: '/food-calorie' ,icon:MdOutlineFoodBank},
+    { label: 'Doc Scanner', href: '/scanner',icon:MdOutlineDocumentScanner },
 ]
 
 export const getBasePath = (pathname:string) => {
